0d6f1f24ceb395e1d4a496b37e409c255625b0df
Braden Ripple <bradenripple@gmail.com>
2020-01-10 22:06:30 -0800

    setting    setPersonsState({persons}) or rather setting setPersonState(persons) to    setPersonsState({persons}) was what made all the difference, normally persons, in python would be inside what is referred to as a set, but in this case persons is inside an object instead of a set

--- src/App.js
+++ src/App.js
@@ -1,63 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.names = {"Tom":"22","Dick":"30","Harry":"45"};
-    for  (const key of Object.keys(this.names)) {
-      this.state.persons.push({name:key, age: this.names[key]})
-    }
-  }
+const app = (props) => {
+  
+  const [personsState,setPersonsState] = useState({
+    persons : []
+  })
+  var names = {"Tom":22,"Dick":30,"Harry":45,"Jesus":30,"Joseph":48,"Mary":Infinity};
+  if (Array.isArray( personsState.persons) && personsState.persons.length == 0) 
+  {
+    personsState.persons = Object.keys(names).map(key => Object({"name":key,"age":names[key]}))}
+  
 
   
-  state = { persons :[]}
-  switchNameHandler = () => {
-   // console.log('was clicked')
-   var persons = this.state.persons
+  function switchNameHandler() {
+    var persons = personsState.persons
    if  ( persons[0].name !== "Tom"){
-     persons[0].name = "Tom";
-      persons[0].age = 25
+     persons[0] = {name:"Tom", age: 25 }
    }
    else {
-     persons[0].name = "Maximiliano"
-     persons[0].age = 27
+    persons[0] = {name:"Maximilian", age: 29 }
+
    }
-  
-  this.setState(persons);
 
+   setPersonsState({persons})
   }
-
-  render() {
     var rows = []
- 
-    for (var obj of this.state.persons){
+    for (var obj of personsState.persons){
       rows.push(<Person name={obj.name} age= {obj.age}></Person>)
     }
     
-
-
      return(
       <div className="App">
         <h1>hi i'm a react app </h1>
         <p> this works!</p>
-        <button onClick={this.switchNameHandler}> Switch Name</button>
+        <button onClick={switchNameHandler}> Switch Name</button>
        <div>{rows} </div>   
       </div>
 
       )
-        /* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */
-     //return React.createElement('div',{className:"App"}, React.createElement('h1',null,'hey that null was needed!'));
-  
-  
+     
 }
-}
-export default App;
+
+export default app;
